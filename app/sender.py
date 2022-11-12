addf8425ef51c3a86b7d267af7dacfd1eb603d7e
natanmacielc <natan.macielc@gmail.com>
2022-11-12 00:01:25 -0300

    final project

---
+++ app/sender.py
@@ -0,0 +1,41 @@
+import psycopg2
+import redis
+import json
+import os
+from bottle import Bottle, request
+
+class Sender(Bottle):
+    def __init__(self):
+        super().__init__()
+        self.route('/', method='POST', callback=self.send)
+        self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
+
+        db_host = os.getenv('DB_HOST', 'db')
+        db_user = os.getenv('POSTGRES_USER', 'postgres')
+        db_password = os.getenv('POSTGRES_PASSWORD', 'postgres')
+        db_name = os.getenv('DB_NAME', 'email_sender')
+        dsn = f'dbname={db_name} user={db_user} password={db_password} host={db_host}'
+        self.conn = psycopg2.connect(dsn)
+
+    def register_message(self, assunto, mensagem):
+        SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)' 
+        cur = self.conn.cursor()
+        cur.execute(SQL, (assunto, mensagem))
+        self.conn.commit()
+        cur.close()
+
+        msg = {'assunto': assunto, 'mensagem': mensagem}
+        self.fila.rpush('sender', json.dumps(msg))
+
+        print('Mensagem registrada !')
+
+    def send(self):
+        assunto = request.forms.get('assunto')
+        mensagem = request.forms.get('mensagem')
+        self.register_message(assunto, mensagem)
+        return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
+        assunto, mensagem)
+
+if __name__ == '__main__':
+    sender = Sender()
+    sender.run(host='0.0.0.0', port=8080, debug=True)
